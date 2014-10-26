7bd1089b9917c6acf16cd65a6e344a7644def0f3
Brycen Dorsay <dorsay@live.ca>
2014-10-26 03:47:52 -0700

    Fit both the counter and the menu on the screen
    Also struggled to get the counter to fit both x and y window size ranges

--- js/main.js
+++ js/main.js
@@ -5,9 +5,29 @@ $("#canvas").click(function(event) {
     updateCount();
 });
 
+$(function() {
+
+    if (window.innerHeight <= window.innerWidth) {
+        $("#count").css({'font-size':'100vh'});
+    } else {
+        $("#count").css({'font-size':'100vw'});
+    }
+});
+
 function updateCount() {
     var countDiv = document.getElementById("count");
     var count = countDiv.innerHTML;
     count++;
     countDiv.innerHTML = count;
 }
+
+try {
+    $(window).resize(function () {
+
+        if (window.innerHeight <= window.innerWidth) {
+            $("#count").css({'font-size': '60vh'});
+        } else {
+            $("#count").css({'font-size': '50vw'})
+        }
+    });
+}catch(err){alert(err.message)}
