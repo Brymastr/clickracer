229fecee13d6dbad177d4ec218d1959a5917572a
Brycen Dorsay <dorsay@live.ca>
2014-11-04 02:05:43 -0800

    Configuration changes

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'A top secret key appears!',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
